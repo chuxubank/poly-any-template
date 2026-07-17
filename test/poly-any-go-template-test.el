d6bd66d4a014c84e8d3a9ec465ee0d19f2c8f744
Misaka <chuxubank@qq.com>
2026-07-17 18:59:24 +0800

    test: split Jinja2 and Go template tests

--- test/poly-any-go-template-test.el
+++ test/poly-any-go-template-test.el
@@ -1,33 +1,16 @@
-;;; poly-any-template-test.el --- Tests for poly-any-template -*- lexical-binding: t; -*-
+;;; poly-any-go-template-test.el --- Tests for poly-any-go-template -*- lexical-binding: t; -*-
 
 ;;; Code:
 
 (require 'ert)
 (require 'poly-any-template)
-(require 'poly-any-jinja2)
 (require 'poly-any-go-template)
 
-(ert-deftest poly-any-template-detects-host-mode ()
-  (let ((auto-mode-alist '(("\\.host\\'" . text-mode))))
-    (should (eq (poly-any-template--get-major-mode-for-file
-                 "/tmp/deployment.host")
-                'text-mode))))
-
-(ert-deftest poly-any-template-configures-jinja2-inner-mode ()
-  (with-temp-buffer
-    (setq buffer-file-name "/tmp/config.text.j2")
-    (poly-any-jinja2-mode)
-    (should polymode-mode)
-    (should (cl-some
-             (lambda (innermode)
-               (eq (eieio-oref innermode 'mode) 'jinja2-mode))
-             (eieio-oref pm/polymode '-innermodes)))))
-
-(ert-deftest poly-any-template-configures-go-inner-mode ()
+(ert-deftest poly-any-go-template-configures-inner-mode ()
   (should (eq (eieio-oref poly-any-template-go-innermode 'mode)
               'go-template-ts-mode)))
 
-(ert-deftest poly-any-template-registers-file-patterns-in-order ()
+(ert-deftest poly-any-go-template-registers-file-patterns-in-order ()
   (let ((poly-entry (cl-position '("\\.[^./]+\\.gotmpl\\'"
                                   . poly-any-go-template-mode)
                                  auto-mode-alist :test #'equal))
@@ -35,12 +18,9 @@
                                   auto-mode-alist :test #'equal)))
     (should poly-entry)
     (should plain-entry)
-    (should (< poly-entry plain-entry))
-    (should (member '("\\.\\(?:j2\\|jinja2\\)\\'"
-                     . poly-any-jinja2-mode)
-                    auto-mode-alist))))
+    (should (< poly-entry plain-entry))))
 
-(ert-deftest poly-any-template-go-span-includes-delimiters ()
+(ert-deftest poly-any-go-template-span-includes-delimiters ()
   (with-temp-buffer
     (setq buffer-file-name "/tmp/deployment.text.gotmpl")
     (insert "name={{ printf \"%s\" .Name }}")
@@ -53,7 +33,7 @@
                       (nth 1 span) (nth 2 span))
                      "{{ printf \"%s\" .Name }}")))))
 
-(ert-deftest poly-any-template-fontifies-host-and-go-inner-mode ()
+(ert-deftest poly-any-go-template-fontifies-host-and-inner-mode ()
   (skip-unless (and (fboundp 'yaml-ts-mode) (treesit-ready-p 'yaml)
                     (treesit-ready-p 'gotmpl)))
   (with-temp-buffer
@@ -75,5 +55,5 @@
     (should (eq (get-text-property (1- (point)) 'face)
                 'font-lock-builtin-face))))
 
-(provide 'poly-any-template-test)
-;;; poly-any-template-test.el ends here
+(provide 'poly-any-go-template-test)
+;;; poly-any-go-template-test.el ends here
---
+++ test/poly-any-jinja2-test.el
@@ -0,0 +1,31 @@
+;;; poly-any-jinja2-test.el --- Tests for poly-any-jinja2 -*- lexical-binding: t; -*-
+
+;;; Code:
+
+(require 'ert)
+(require 'poly-any-template)
+(require 'poly-any-jinja2)
+
+(ert-deftest poly-any-jinja2-detects-host-mode ()
+  (let ((auto-mode-alist '(("\\.host\\'" . text-mode))))
+    (should (eq (poly-any-template--get-major-mode-for-file
+                 "/tmp/config.host")
+                'text-mode))))
+
+(ert-deftest poly-any-jinja2-configures-inner-mode ()
+  (with-temp-buffer
+    (setq buffer-file-name "/tmp/config.text.j2")
+    (poly-any-jinja2-mode)
+    (should polymode-mode)
+    (should (cl-some
+             (lambda (innermode)
+               (eq (eieio-oref innermode 'mode) 'jinja2-mode))
+             (eieio-oref pm/polymode '-innermodes)))))
+
+(ert-deftest poly-any-jinja2-registers-file-pattern ()
+  (should (member '("\\.\\(?:j2\\|jinja2\\)\\'"
+                   . poly-any-jinja2-mode)
+                  auto-mode-alist)))
+
+(provide 'poly-any-jinja2-test)
+;;; poly-any-jinja2-test.el ends here
